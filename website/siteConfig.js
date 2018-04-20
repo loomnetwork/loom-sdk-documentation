d63d82b1c084146c5e9cc4c5bf1b662d20f86325
Jim Skogman <yim.apichai@gmail.com>
2018-04-20 16:13:32 +0700

    Initial commit

---
+++ website/siteConfig.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config.html for all the possible
+// site configuration options.
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'User1',
+    image: 'img/zombie-dude.png',
+    infoLink: 'https://loomx.io/',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'Loom SDK' /* title for your website */,
+  tagline: 'Build decentralized worlds',
+  url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'Loom SDK',
+  organizationName: 'Loom Network',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/loom-logo.svg',
+  footerIcon: 'img/loom-logo.svg',
+  favicon: 'img/favicon.png',
+
+  /* colors for website */
+  colors: {
+    primaryColor: '#2B72BA',
+    secondaryColor: '#64B7E2',
+  },
+
+  /* custom fonts for website */
+  /*fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },*/
+
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' Loom Network',
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  /* On page navigation for the current documentation page */
+  onPageNav: 'separate',
+
+  /* Open Graph and Twitter card images */
+  ogImage: 'img/docusaurus.png',
+  twitterImage: 'img/docusaurus.png',
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
