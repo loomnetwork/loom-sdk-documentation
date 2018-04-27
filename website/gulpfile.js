9d0b566f7bad43b7bb10fbec3798e2c3226550b6
Jim Skogman <yim.apichai@gmail.com>
2018-04-27 18:45:56 +0700

    Add callback to gulp tasks

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -7,6 +7,46 @@ var sourcemaps = require('gulp-sourcemaps');
 var hash = require('gulp-hash');
 var replace = require('gulp-replace');
 
+gulp.task('css', function(){
+  return gulp.src('build/developers/css/main.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('build/developers/css/main.min.css'))
+});
+
+gulp.task('hash', function(cb){
+  return gulp.src('build/developers/css/main.css')
+    .pipe(hash())
+    .pipe(gulp.dest('build/developers/css'))
+    .pipe(hash.manifest('build/developers/assets.json', {
+    	  deleteOld: true,
+    	  sourceDir: __dirname + '/build/developers'
+    	}))
+    .pipe(gulp.dest('.'));
+});
+
+
+gulp.task('replace', ['hash'], function(){
+  var manifest = JSON.parse(fs.readFileSync(__dirname + '/build/developers/assets.json', 'utf8'));
+  gulp.src(['build/developers/*.html'])
+    .pipe(replace('main.css', manifest['main.css']))
+    .pipe(gulp.dest('build/developers'));
+});
+
+gulp.task('replaceDocs', ['hash'], function(){
+  var manifest = JSON.parse(fs.readFileSync(__dirname + '/build/developers/assets.json', 'utf8'));
+  gulp.src(['build/developers/docs/*.html'])
+    .pipe(replace('main.css', manifest['main.css']))
+    .pipe(gulp.dest('build/developers/docs'));
+});
+
+gulp.task('replaceDocsEn', ['hash'], function(){
+  var manifest = JSON.parse(fs.readFileSync(__dirname + '/build/developers/assets.json', 'utf8'));
+  gulp.src(['build/developers/docs/en/*.html'])
+    .pipe(replace('main.css', manifest['main.css']))
+    .pipe(gulp.dest('build/developers/docs/en'));
+});
+
+
 gulp.task('viewPort', function() {
   return gulp.src('build/developers/index.html')
     .pipe(dom(function(){
@@ -23,37 +63,4 @@ gulp.task('viewPortEn', function() {
     .pipe(gulp.dest('build/developers/en'));
 });
 
-gulp.task('css', function(){
-  return gulp.src('build/developers/css/main.css')
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('build/developers/css/main.min.css'))
-});
-
-gulp.task('hash', function(){
-  return gulp.src('build/developers/css/main.css')
-    .pipe(hash())
-    .pipe(gulp.dest('build/developers/css'))
-    .pipe(hash.manifest('build/developers/assets.json', {
-    	  deleteOld: true,
-    	  sourceDir: __dirname + '/build/developers'
-    	}))
-    .pipe(gulp.dest('.'));
-
-    var manifest = JSON.parse(fs.readFileSync(__dirname + '/build/developers/assets.json', 'utf8'));
-
-    gulp.src(['build/developers/*.html'])
-      .pipe(replace('main.css', manifest['main.css']))
-      .pipe(gulp.dest('build/developers'));
-
-    gulp.src(['build/developers/docs/*.html'])
-      .pipe(replace('main.css', manifest['main.css']))
-      .pipe(gulp.dest('build/developers/docs'));
-
-    gulp.src(['build/developers/docs/en/*.html'])
-      .pipe(replace('main.css', manifest['main.css']))
-      .pipe(gulp.dest('build/developers/docs/en'));
-
-});
-
-
-gulp.task('default', [ 'css', 'hash', 'viewPort', 'viewPortEn' ]);
+gulp.task('default', [ 'css', 'hash', 'viewPort', 'viewPortEn', 'replace', 'replaceDocs', 'replaceDocsEn' ]);
