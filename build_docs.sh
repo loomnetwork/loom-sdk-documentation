d8773a84e5c02b2b681cbabffc26a20801396b64
Matthew Campbell <hyper@hyperworks.nu>
2018-10-02 18:34:56 +0700

    deploy docs to staging server

--- build_docs.sh
+++ build_docs.sh
@@ -8,4 +8,9 @@ yarn
 cp i18n/ja-JP.json i18n/ja.json
 yarn run build
 
+if [ "$GIT_BRANCH" = "origin/master" ]
+then
 aws s3 cp build/developers s3://loomx.io/developers  --recursive --cache-control 'max-age=86400'
+else
+aws s3 cp build/developers s3://stage.loomx.io/developers  --recursive --cache-control 'max-age=86400'
+fi
