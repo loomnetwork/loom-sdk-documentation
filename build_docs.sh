b73ce1405e68333324defaa5b6b7c14d655ea2a2
Luke Zhang <luke@lukezhang.ca>
2019-05-14 17:43:16 +0800

    enable building for master

--- build_docs.sh
+++ build_docs.sh
@@ -8,8 +8,7 @@ yarn run build
 
 if [ "$GIT_BRANCH" = "origin/master" ]
 then
-# aws s3 cp .vuepress/dist s3://loomx.io/developers  --recursive --cache-control 'max-age=86400'
-echo 'Not building for master'
+aws s3 cp ./docs/.vuepress/dist s3://loomx.io/developers  --recursive --cache-control 'max-age=86400'
 else
 aws s3 cp ./docs/.vuepress/dist s3://stage.loomx.io/developers  --recursive --cache-control 'max-age=86400'
 fi
