c716c8a5013eb4e6fe4974b3a42b83f4e4fbdbb3
Hank X <yrstyh@gmail.com>
2019-08-12 15:04:48 +0800

    trigger build

--- build_docs.sh
+++ build_docs.sh
@@ -12,5 +12,6 @@ if [ "$GIT_BRANCH" = "origin/master" ]
 then
 aws s3 cp ./docs/.vuepress/dist s3://loomx.io/developers  --recursive --cache-control 'max-age=86400'
 else
+#aws s3 rm s3://stage.loomx.io/developers --recursive #remove old files first
 aws s3 cp ./docs/.vuepress/dist s3://stage.loomx.io/developers  --recursive --cache-control 'max-age=86400'
 fi
